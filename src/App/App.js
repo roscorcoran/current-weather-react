539223dc17a29ecc163831a23b7351d7ebb5d98f
ros <roscorcoran@gmail.com>
2019-05-09 11:51:18 +0100

    Refactor App component and strip unnecessary create-react-app code and assets

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                Current Weather in React....
+            </div>
+        );
+    }
+}
+
+export default App;
